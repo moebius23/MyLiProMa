c66afce449acb6fb92c7e1a6b9056fd1f4c55df8
Augustin Bocken <augustin.bocken@hotmail.com>
2018-04-21 20:59:50 +0200

    Creating store

---
+++ MyLittleProjectManager/MyLittleProjectManager/Models/StoreViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLittleProjectManager.Models
+{
+    public class StoreViewModel
+    {
+        public PlayerProfile Profile { get; set; }
+        public List<Item> StoreItems { get; set; }
+        
+        public List<Item> BoughtItems { get; set; }
+    }
+}
