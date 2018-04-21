3644cc93474beeb1909ab63d2feedcebbb8553f4
Morgan0018 <morgandreze@gmail.com>
2018-04-21 14:45:22 +0200

    Mod index()

--- MyLittleProjectManager/MyLittleProjectManager/Controllers/ProfileController.cs
+++ MyLittleProjectManager/MyLittleProjectManager/Controllers/ProfileController.cs
@@ -4,18 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyLittleProjectManager.Data;
 using MyLittleProjectManager.Models;
 
 namespace MyLittleProjectManager.Controllers
 {
     public class ProfileController : Controller
     {
-        [HttpGet]
+		ApplicationDbContext context = new ApplicationDbContext(null);
+
+		[HttpGet]
         [Authorize]
         public IActionResult Index()
         {
-            PlayerProfile pp = new PlayerProfile();
-            pp.Pseudo = "Salsify";
+			var user = context.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+			PlayerProfile pp = user.PlayerProfile;
             return View(pp);
         }
     }
--- MyLittleProjectManager/MyLittleProjectManager/Controllers/ProjectController.cs
+++ MyLittleProjectManager/MyLittleProjectManager/Controllers/ProjectController.cs
@@ -3,47 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyLittleProjectManager.Data;
 using MyLittleProjectManager.Models;
 
 namespace MyLittleProjectManager.Controllers
 {
     public class ProjectController : Controller
     {
+		ApplicationDbContext context = new ApplicationDbContext(null);
+
         public IActionResult Index()
         {
-            Project project = new Project()
-            {
-                Id = 1,
-                Name = "MyLittleProjectManager",
-                Columns = new System.Collections.ObjectModel.ObservableCollection<Column>()
-                {
-                    new Column()
-                    {
-                        Id =1,
-                        Name ="To Do",
-                        Order =0,
-                        Cards = new System.Collections.ObjectModel.ObservableCollection<Card>()
-                        {
-                            new Card()
-                            {
-                                Id = 0,
-                                Order = 0,
-                                Name = "Create Data",
-                                Description = "All your data are belong to us !"
-                            },
-                            new Card()
-                            {
-                                Id = 1,
-                                Order = 1,
-                                Name = "Process Data",
-                                Description = "An army of monkeys is working on the data."
-                            }
-                        }
-                    },
-                    new Column(){Id=2,Name="Doing",Order=1},
-                    new Column(){Id=3,Name="Done",Order=2}
-                }
-            };
+			Project project = context.Projects.FirstOrDefault();
             return View(project);
         }
 
@@ -52,43 +23,9 @@ namespace MyLittleProjectManager.Controllers
             Console.WriteLine(String.Format("Moving card {0} to column {1}",CardId,NewColumnId));
         }
 
-        public List<Column> Columns()
+        public List<Column> Columns(int projectId)
         {
-            return new List<Column>()
-                {
-                    new Column()
-                    {
-                        Id =1,
-                        Name ="To Do",
-                        Order =0,
-                        Cards = new System.Collections.ObjectModel.ObservableCollection<Card>()
-                        {
-                            new Card()
-                            {
-                                Id = 0,
-                                Order = 0,
-                                Name = "Create Data",
-                                Description = "All your data are belong to us !"
-                            },
-                            new Card()
-                            {
-                                Id = 1,
-                                Order = 1,
-                                Name = "Process Data",
-                                Description = "An army of monkeys is working on the data."
-                            }
-                        }
-                    },
-                    new Column(){Id=2,Name="Doing",Order=1},
-                    new Column(){Id=3,Name="Done",Order=2,
-                    Cards = new System.Collections.ObjectModel.ObservableCollection<Card>(){ new Card()
-                            {
-                                Id = 2,
-                                Order = 0,
-                                Name = "Manage Data",
-                                Description = "Because it is easier to manage when there is none."
-                            }} }
-                };
+			return context.Columns.Where(p => p.Id == projectId).ToList();
         }
     }
 }
